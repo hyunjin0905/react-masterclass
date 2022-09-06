7ba95e3c8509a3e2b50ea376d0ea0aa7a2724c41
lucy <kkadangj@gmail.com>
2022-09-06 12:54:55 +0900

    # theme type def 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import Circle from "./Circle";
+import styled from "styled-components";
 
-
+const Container = styled.div`
+    background-color: ${props => props.theme.bgColor}
+`
+const H1 = styled.h1`
+    color: ${props => props.theme.textColor}
+`
 function App() {
   return(
-      <div>
-        <Circle bgColor="teal" borderColor="yellow"/>
-        <Circle bgColor="teal" text="hello"/>
-      </div>
+      <Container>
+        <H1>protected</H1>
+      </Container>
   )
 }
 
