dfa2a1f127f48145d3e39fa8571bafdf34e9c03c
lucy <kkadangj@gmail.com>
2022-09-07 11:23:31 +0900

    # react-router-dom 세팅

--- src/theme.ts
+++ src/theme.ts
@@ -1,12 +1,8 @@
 import { DefaultTheme } from "styled-components";
 
-export const lightTheme: DefaultTheme = {
-    bgColor: "white",
-    textColor: "black"
+export const theme: DefaultTheme = {
+    bgColor: "#2f3640",
+    textColor: "#f5f6fa",
+    accentColor: "#44bd32"
 }
 
-export const darkTheme: DefaultTheme = {
-    bgColor: "black",
-    textColor: "white"
-
-}
